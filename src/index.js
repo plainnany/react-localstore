bf594a989e6ebab7fbd013b60275fbd76413d18b
plainnany <plainnany@gmail.com>
2017-11-13 14:52:37 +0800

    add comment

--- src/index.js
+++ src/index.js
@@ -4,19 +4,20 @@ import App from './App';
 
 ReactDOM.render(<App />,document.getElementById('root'));
 
-
-
-// function tick() {
-//     const element = (
-//       <div>
-//         <h1>Hello, world!</h1>
-//         <h2>It is {new Date().toLocaleTimeString()}.</h2>
-//       </div>
-//     );
-//     ReactDOM.render(
-//       element,
-//       document.getElementById('root')
-//     );
-//   }
+/**
+ * 
+function tick() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    ReactDOM.render(
+      element,
+      document.getElementById('root')
+    );
+  }
   
-// setInterval(tick, 1000);
+setInterval(tick, 1000);
+ */
